cc146c5ade8700e4a1d09ee47ed92726611f13b2
ThomasP-Weblimitless <thomas@algbmail.com>
2019-07-05 18:26:49 +0100

    Initial commit

---
+++ src/components/Calendar/Calendar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Calendar = props => {
+  return (
+    <div>
+      <h1>Year 2019</h1>
+
+      <table />
+    </div>
+  );
+};
+
+Calendar.propTypes = {};
+
+export default Calendar;
--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from '~/components/App';
+import 'react-testing-library/cleanup-after-each';
+import 'jest-dom/extend-expect';
 
-it('renders without crashing', () => {
+test('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
